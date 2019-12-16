2feda6f841ee1359e840f29f6f2f5648f3277f4e
codercwm <987950059@qq.com>
2019-12-16 21:51:44 +0800

    功能完善

--- src/QueueExport.php
+++ src/QueueExport.php
@@ -725,6 +725,11 @@ class QueueExport{
 
     //获取用点分隔的多维数组的值
     private function getDotValue($item,$field){
+        if('`'==substr($field,0,1)&&'`'==substr($field,-1,1)){
+            //如果有``包围的就直接返回
+            $value = trim($field,'`');
+            return $value;
+        }
         $field = explode('.',$field);
         $value = $item;
         foreach ($field as $f){
@@ -766,37 +771,41 @@ class QueueExport{
                 }
 
                 $value = $field;
-                //看有没有函数，有就调用函数
-                $func_name = substr($value,0,(strpos($value,'(')));//func();
-                if(strpos($value,'(')&&strpos($value,')')&&function_exists($func_name)){
-                    $sub_start = strpos($value,'(');
-                    $sub_end = strripos($value,')');
-                    $func_param_str = substr($value,$sub_start+1,$sub_end-$sub_start-1);
-                    $func_param_arr = explode(',',$func_param_str);
-                    $func_param_arr = array_map(function($va){return str_replace(['"',"'"],'',$va);},$func_param_arr);
-
-                    $dot_value_arr = [];
-                    foreach ($func_param_arr as $func_param){
-                        $dot_value_arr[] = $this->getDotValue($item,$func_param);
-                    }
-                    $value = call_user_func_array($func_name,$dot_value_arr);
+                if('`'==substr($value,0,1)&&'`'==substr($value,-1,1)){
+                    //如果有``包围的就直接取值
+                    $value = trim($value,'`');
                 }else{
-                    $value = $this->getDotValue($item,$value);
-                }
+                    //看有没有函数，有就调用函数
+                    $func_name = substr($value,0,(strpos($value,'(')));//func();
+                    if(strpos($value,'(')&&strpos($value,')')&&function_exists($func_name)){
+                        $sub_start = strpos($value,'(');
+                        $sub_end = strripos($value,')');
+                        $func_param_str = substr($value,$sub_start+1,$sub_end-$sub_start-1);
+                        $func_param_arr = explode(',',$func_param_str);
+                        $func_param_arr = array_map(function($va){return str_replace(['"',"'",],'',$va);},$func_param_arr);
+                        $func_param_arr = array_map('trim',$func_param_arr);
 
-                //如果有字典
-                if(!empty($dict_arr)&&isset($dict_arr[$value])){
-                    $value = $dict_arr[$value];
+                        $dot_value_arr = [];
+                        foreach ($func_param_arr as $func_param){
+                            $dot_value_arr[] = $this->getDotValue($item,$func_param);
+                        }
+                        $value = call_user_func_array($func_name,$dot_value_arr);
+                    }else{
+                        $value = $this->getDotValue($item,$value);
+                    }
+                    //如果有字典
+                    if(!empty($dict_arr)&&isset($dict_arr[$value])){
+                        $value = $dict_arr[$value];
+                    }
+                    //如果单元格的值是数组就转成json
+                    if(is_array($value)){
+                        $value = $this->enJson($value);
+                    }
+                    //如果添加制表符，就是转换成字符串，不用科学计数法显示
+                    if($tabs || is_object($value)){
+                        $value = "\t".$value."\t";
+                    }
                 }
-                //如果单元格的值是数组就转成json
-                if(is_array($value)){
-                    $value = $this->enJson($value);
-                }
-                //如果添加制表符，就是转换成字符串，不用科学计数法显示
-                if($tabs || is_object($value)){
-                    $value = "\t".$value."\t";
-                }
-
             }
 
             $data[] = $value;
